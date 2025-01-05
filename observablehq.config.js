cbc93da8a2133ad8eecfa8c231d1591eb89ed20f
zachcp <zachcp@users.noreply.github.com>
2025-01-05 13:43:10 -0500

    20250105 plm dash (#1)
    * factor to component
    
    * delete unused tempalte files.

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,7 +17,7 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="app.png" type="image/png" sizes="30x30">',
 
   // The path to the source root.
   root: "src",
@@ -26,9 +26,9 @@ export default {
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // sidebar: true, // whether to show the sidebar
+  sidebar: false, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
+  pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
